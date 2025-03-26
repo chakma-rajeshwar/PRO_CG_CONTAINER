809e73073c15e03deee54bc8be93554d65570b24
chakma-rajeshwar <rajchakma95@gmail.com>
2025-03-26 21:11:44 +0600

    commit initialize

---
+++ frontend/components/Example.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Example = () => {
+  return (
+    <div className="example">
+      <p>This is an example component</p>
+    </div>
+  );
+};
+
+export default Example;
