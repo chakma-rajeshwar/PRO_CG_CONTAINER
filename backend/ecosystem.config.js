9e3379afcc1407de8b65c9e7128b758a3dbcb7c8
chakma-rajeshwar <108381744+chakma-rajeshwar@users.noreply.github.com>
2025-03-26 21:00:17 +0600

    Update ecosystem.config.js

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -2,14 +2,12 @@ module.exports = {
   apps: [
     {
       name: "node-app",
-      cwd: "/app/node",
       script: "node",
       args: "app.js",
       watch: true
     },
     {
       name: "flask-app",
-      cwd: "/app/flask",
       script: "python",
       args: "app.py",
       watch: true
